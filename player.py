d3087a57abaa558f81da33b978fccd23ddca73cd
TeddyBerhane <tb1243@txstate.edu>
2013-09-24 13:10:30 -0500

    Didn't need the methods to print values. Could be accessed from Player object.

--- player.py
+++ player.py
@@ -5,9 +5,8 @@ class Player(object):
         self.inv = {}
         self.ammo = 0
         self.points = 0
-
-    def printHealth(self):
-        print self.health
+        self.gun = False
+        self.gunDamage = 0
 
     def addToInv(self, name, coord):
         self.inv[name] = coord
@@ -19,8 +18,24 @@ class Player(object):
     def increaseHealth(self, amount):
         if self.health < 100:
             self.health = min((self.health + amount), 100)
-            return self.health
 
     def increasePoints(self, amount):
         self.points += amount
-        return self.points
+
+    def increaseAmmo(self, amount):
+        self.ammo += amount
+
+    def decreaseAmmo(self):
+        if self.ammo > 0:
+            self.ammo = max((self.ammo - 1), 0)
+
+    def addToGunDamage(self, amount):
+        self.gunDamage = amount
+
+    def useGun(self):
+        self.gun = True
+        return True
+
+    def putUpGun(self):
+        self.gun = False
+        return False
